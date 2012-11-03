c95da2ce0f10191417e63e61c2d4d9b35aa5de15
Andrew Dennis <adennis@groupon.com>
2012-11-03 15:43:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MechTurk::Application.config.secret_token = 'f2609a1ba4b687382fbb52dcdaf698d70d8923302fdc4269ecbacc253c9d2751f9056d8868d79a5356fa3a984fdb6736ac8a01a586c57fa5156a0c54e90509ca'
