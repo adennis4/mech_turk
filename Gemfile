853be80f1c9aebba47ec5bbd1d83f08e3e1ebcd2
Andrew Dennis <adennis@groupon.com>
2012-11-16 16:27:07 -0600

    Revert "adds devise gem and config"
    This reverts commit b871886a0b5702f6b168f97b638bd7bc2705db29.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.6'
 gem 'haml-rails', '~> 0.3.4'
 gem 'pg'
-gem 'devise'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,4 @@ MechTurk::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
-  #Default url options defined for devise gem
-  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
 end
